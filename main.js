f46027055da88101d1c98a4a556ac1fb4cff9e0b
Praktikant <praktikant@athena.local>
2023-02-07 15:00:39 +0100

    top10 scoreboard anfang

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ window.reaktionsBtn = reaktionsBtn;
 var randomNumberOutput;
 
 function reaktionsBtn() {
-  randomNumberOutput = Math.floor(Math.random() * 5000);
+  randomNumberOutput = Math.floor(Math.random() * (5000 - 1000) + 1000);
   console.log(randomNumberOutput);
   setTimeout(reaktionsClick, randomNumberOutput);
   
 }
 var scoreboard = [];
-
+var top_10 = [];
 
 
 // scoreboard.forEach(createHTML);
@@ -33,14 +33,25 @@ function reaktionsClick() {
     let date2 = new Date();
     var time_later = date2.getTime();
     const timeResult = (time_later - time_now);
-    scoreboard.push(timeResult);
+    scoreboard.push(timeResult);  
     scoreboard.sort(function (a, b) { return a - b });
-    document.getElementById("Scoreboard").innerHTML = JSON.stringify(scoreboard);
+    for (let i = 0; (i <=10 || i < scoreboard.length -1 ); i++){
+      console.log("scoreboard länge",scoreboard.length);
+      top_10[i] = scoreboard[i];
+       top_10.filter(obj => obj);
+       console.log("top_10[i]",top_10[i]);
+    //  top_10.filter(Element => Element !== null);
+    }
+    // if (top_10.length >=11){
+    //   top_10.splice(10, 1);
+    // }
+    
+    document.getElementById("Top10").innerHTML = JSON.stringify(top_10);
     console.log(scoreboard);
     document.getElementById("timerResult").innerHTML = timeResult + "ms";
     document.getElementById("ReaktionsTest").style.background = "#f5f5f5";
     document.getElementById("ReaktionsTest").innerHTML = "Drücke diesen button wenn er grün ist"
-    document.getElementById("ReaktionsButton").innerHTML = "Start Game"
+    // document.getElementById("ReaktionsButton").innerHTML = "Start Game"
     console.log(timeResult);
     ReaktionsTest.removeEventListener("click", Cal_Result)
   }
@@ -74,5 +85,9 @@ function reaktionsClick() {
 // mach eine function die das game neustartet ohne die website zu restarten                ++++++++DONE+++++++++
 // Wenn alles klappt dann versuchen ein scoreboard zu machen was die 10 besten runs mit  und milisekunden trackt
 // Versuch das score board entwieder rechts oder links and die seite zu stellen
-// Ein reset button ist nötig für das erneue spielen !!Aber das scoreboard und die einträge müssen noch bleiben!!
 // Wenn das scoreboard funktoniert dann verschöner die website mit css 
+  
+
+
+// Design pläne: scoreboard an die seie packen mit einen gutem hintergrund 
+// versuchen das so zu machen das der array scoreboard all in einem colum gepusht werden, dazu auch verschieden arrays zu machen für z. score 1 score 2 usw.
---
+++ reaktionsSpiel Vorstellung.md
@@ -0,0 +1,16 @@
+Vorstellung: name,schule und klasse , praktikum, , interessen, vorkentnisse.
+1 Aufgabe: Ein ReaktionsSpiel zu coden mit einen anständigen Scoreboard und design
+2 "Materialen": in vs Code zu coden mit Javascript, HTML und CSS. Terminal. Git/github.
+3 Vorgangsweise: 
+4 probleme:
+5 Schluss: fazit
+
+
+Notizen und pläne:
+Bei interessen erkären warum du dich für das interessiert (mit velleicht ein bespiel)
+Bei Vorkenntnisse sag das du keine vorkenntnisse hattest aber hier programmieren gelernt hast
+Bei Aufgabe zeig das scoreboard links (falls fertig) und das end design
+Bei Materiallen erkläre was was ist und beim terminal und git erklären wie du das auf git gespeichert hast
+Bei Vorgangsweise erkläre womit du angefangen hast wie du vorgegangen bist aber es ein bisschen grob erklären
+Bei probleme erkäre was für große probleme du hattest beim coden und warum dies ein problem war 
+
